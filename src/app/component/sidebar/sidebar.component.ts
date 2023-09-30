36f18e1c4bc39e16819a4cededec464547e6dbba
tanggyyggnat <khunthida1244@gmail.com>
2023-10-01 01:45:56 +0700

    create new component for sidebar

---
+++ src/app/component/sidebar/sidebar.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        items: [
+          {
+            label: 'Home',
+            icon: 'pi pi-home',
+            routerLink: ['/home'],
+          },
+          {
+            label: 'Booking',
+            icon: 'pi pi-book',
+            routerLink: ['/booking'],
+          },
+          {
+            label: 'Process',
+            icon: 'pi pi-file',
+            routerLink: ['/process'],
+          },
+          {
+            label: 'History',
+            icon: 'pi pi-history',
+            routerLink: ['/history'],
+          },
+          {
+            label: 'Payment',
+            icon: 'pi pi-dollar',
+            routerLink: ['/payment'],
+          },
+        ],
+      },
+
+      // {
+      //   items: [
+      //     {
+      //       label: 'Home',
+      //       icon: 'pi pi-home',
+      //       routerLink: ['/menu'],
+      //     },
+      //     {
+      //       label: 'Schedule',
+      //       icon: 'pi pi-calendar',
+      //       routerLink: ['/schedule'],
+      //     },
+      //     {
+      //       label: 'Inventory',
+      //       icon: 'pi pi-inbox',
+      //       routerLink: ['/inventory'],
+      //     },
+      //     {
+      //       label: 'Reservation',
+      //       icon: 'pi pi-ticket',
+      //       routerLink: ['/reservation'],
+      //     },
+      //   ],
+      // },
+    ];
+  }
+
+  signOut() {
+    // Add Sign Out Function
+  }
+}
