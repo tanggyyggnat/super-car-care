c3e2ae51e6f161dc578e54e7e43702f8e7d2c936
Nutnicha Feuangfu <aina-123@hotmail.co.th>
2023-10-01 04:50:48 +0700

    เพิ่มปุ่ม succes กับ in process แล้ว

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -9,23 +9,32 @@ export class HistoryComponent {
   products: any[]=[
     {
       booking:"1",
-      Service:"washingcar",	
-      Date:"10/1/2023",	
-      Time:"2:34 AM",	
-      CustomerName:"Aimakase",	
-      Carbrand:"chocolate",	
-      CarLicense:"ai123",
-      Status:"success"						
+      service:"washingcar",
+      date:"10/1/2023",
+      time:"2:34 AM",
+      customerName:"Aimakase",
+      carbrand:"chocolate",
+      carLicense:"ai123",
+      status:"success"
     },
     {
       booking:"2",
-      Service:"machanic",	
-      Date:"10/2/2023",	
-      Time:"12:34 AM",	
-      CustomerName:"Tanggy",	
-      Carbrand:"strawberry",	
-      CarLicense:"tang789",
-      Status:"in process"						
+      service:"machanic",
+      date:"10/2/2023",
+      time:"12:34 AM",
+      customerName:"Tanggy",
+      carbrand:"strawberry",
+      carLicense:"tang789",
+      status:"in process"
     }
   ];
+
+  getSeverity(status: string):any {
+    switch (status) {
+      case 'success':
+        return 'success';
+      case 'in process':
+        return 'warning';
+    }
+  }
 }
