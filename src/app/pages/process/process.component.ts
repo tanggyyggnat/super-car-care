2e30578cbaa7b70096db34673becaa14063f7231
Nttp <81904457+MaledKhaoSan@users.noreply.github.com>
2023-10-03 23:31:44 +0700

    - เพิ่ม process กับ reservation

---
+++ src/app/pages/process/process.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-process',
+  templateUrl: './process.component.html',
+  styleUrls: ['./process.component.scss']
+})
+export class ProcessComponent {
+  ID: string = '5001'
+  //เดี๋ยวมาใส่เพิ่ม
+}
