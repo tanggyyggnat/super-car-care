f8926b0d81c50686e7ddd0663c730468133ec806
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-08 00:42:00 +0700

    create new component for tab check out

---
+++ src/app/pages/history/history-detail-tab/tab-check-out/tab-check-out.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tab-check-out',
+  templateUrl: './tab-check-out.component.html',
+  styleUrls: ['./tab-check-out.component.scss'],
+  standalone: true,
+})
+export class TabCheckOutComponent {
+
+}
