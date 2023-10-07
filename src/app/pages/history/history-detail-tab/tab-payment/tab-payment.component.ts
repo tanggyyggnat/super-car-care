7cef75d05434802f0d102287f906e02ba85714bb
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-08 00:42:14 +0700

    create new component for tab payment

---
+++ src/app/pages/history/history-detail-tab/tab-payment/tab-payment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tab-payment',
+  templateUrl: './tab-payment.component.html',
+  styleUrls: ['./tab-payment.component.scss'],
+  standalone: true,
+})
+export class TabPaymentComponent {
+
+}
