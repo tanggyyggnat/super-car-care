e27b42408c76622e409206383068195516d974f7
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-08 00:46:17 +0700

    create new component for history detail

--- src/app/pages/history/history-detail/history-detail.component.ts
+++ src/app/pages/history/history-detail/history-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import data from 'src/app/mockup/customer.json';
 
 @Component({
   selector: 'app-history-detail',
@@ -6,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./history-detail.component.scss']
 })
 export class HistoryDetailComponent {
+  activeIndex: number = 0;
+  customerId: string = '';
+  customer: any = {};
 
+  items: MenuItem[] = [
+    {
+      label: 'Booking List',
+      routerLink: '/history',
+    },
+    {
+      label: 'Booking Detail',
+      styleClass: 'font-bold',
+    },
+  ];
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.activatedRoute.params.subscribe(params => {
+      if (params['id']) {
+        this.customerId = params['id'];
+        this.getCustomer();
+      }
+    });
+  }
+
+  getCustomer() {
+    if (!this.customerId) return;
+    data.filter((item: any) => {
+      if (item.id == this.customerId) {
+        this.customer = item;
+      }
+    });
+  }
+
+  changeIndex(index: number) {
+    this.activeIndex = index;
+  }
 }
