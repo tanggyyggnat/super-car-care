38085ca8133acb98163bb2e24f571ba2b942b397
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-08 00:41:51 +0700

    create new component for tab check in

---
+++ src/app/pages/history/history-detail-tab/tab-check-in/tab-check-in.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tab-check-in',
+  templateUrl: './tab-check-in.component.html',
+  styleUrls: ['./tab-check-in.component.scss'],
+  standalone: true,
+})
+export class TabCheckInComponent {
+
+}
