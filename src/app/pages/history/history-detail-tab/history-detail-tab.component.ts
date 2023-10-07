254295ed6d3600cf825e09a323c49ad71eb58334
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-08 00:42:40 +0700

    create new component for tab detail

---
+++ src/app/pages/history/history-detail-tab/history-detail-tab.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { PrimeNGModule } from 'src/app/modules/primeng.module';
+import { TabCheckInComponent } from './tab-check-in/tab-check-in.component';
+import { TabCheckOutComponent } from './tab-check-out/tab-check-out.component';
+import { TabPaymentComponent } from './tab-payment/tab-payment.component';
+import { TabProcessComponent } from './tab-process/tab-process.component';
+
+@Component({
+  selector: 'history-detail-tab',
+  templateUrl: './history-detail-tab.component.html',
+  styleUrls: ['./history-detail-tab.component.scss'],
+  standalone: true,
+  imports: [
+    PrimeNGModule,
+    TabCheckInComponent,
+    TabCheckOutComponent,
+    TabPaymentComponent,
+    TabProcessComponent,
+  ]
+})
+export class HistoryDetailTabComponent {
+  @Input() activeIndex: number = 0;
+}
