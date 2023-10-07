5b5032c6e1cae3cc068d93f133c77161caf5496d
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-08 00:42:25 +0700

    create new component for tab process

---
+++ src/app/pages/history/history-detail-tab/tab-process/tab-process.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tab-process',
+  templateUrl: './tab-process.component.html',
+  styleUrls: ['./tab-process.component.scss'],
+  standalone: true,
+})
+export class TabProcessComponent {
+
+}
