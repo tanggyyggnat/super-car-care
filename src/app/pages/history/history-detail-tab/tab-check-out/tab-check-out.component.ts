b637bbb4023455e13870d6da245f4d1e6cd3d575
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-08 20:23:17 +0700

    done check out page

--- src/app/pages/history/history-detail-tab/tab-check-out/tab-check-out.component.ts
+++ src/app/pages/history/history-detail-tab/tab-check-out/tab-check-out.component.ts
@@ -1,10 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PrimeNGModule } from 'src/app/modules/primeng.module';
 
 @Component({
   selector: 'tab-check-out',
   templateUrl: './tab-check-out.component.html',
   styleUrls: ['./tab-check-out.component.scss'],
   standalone: true,
+  imports: [
+    CommonModule,
+    PrimeNGModule
+  ]
 })
 export class TabCheckOutComponent {
 
