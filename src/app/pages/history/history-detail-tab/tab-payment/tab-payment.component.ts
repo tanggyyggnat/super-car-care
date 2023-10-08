9d24b0fca06087b8d54f8af988d3ce32a9fa7428
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-08 16:00:13 +0700

    done tab payment

--- src/app/pages/history/history-detail-tab/tab-payment/tab-payment.component.ts
+++ src/app/pages/history/history-detail-tab/tab-payment/tab-payment.component.ts
@@ -1,11 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PrimeNGModule } from 'src/app/modules/primeng.module';
 
 @Component({
   selector: 'tab-payment',
   templateUrl: './tab-payment.component.html',
   styleUrls: ['./tab-payment.component.scss'],
   standalone: true,
+  imports: [
+    CommonModule,
+    PrimeNGModule,
+  ]
 })
 export class TabPaymentComponent {
 
+  value: number | undefined;
+  mobilebankingMethod = false;
+  cashMethod = false;
+
+  constructor() {
+    console.log(this.cashMethod)
+    console.log(this.mobilebankingMethod)
+  }
+
+  selected(method: string) {
+    if (method === 'CASH') {
+      this.cashMethod = true;
+      this.mobilebankingMethod = false;
+    } else if (method === 'BANKING') {
+      this.cashMethod = false;
+      this.mobilebankingMethod = true;
+    }
+  }
 }
