fbdba62a57c742ba6599100212de311382d30dba
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-08 20:38:55 +0700

    update

--- src/app/pages/history/history-detail-tab/tab-check-in/tab-check-in.component.ts
+++ src/app/pages/history/history-detail-tab/tab-check-in/tab-check-in.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { PrimeNGModule } from 'src/app/modules/primeng.module';
 
 @Component({
   selector: 'tab-check-in',
   templateUrl: './tab-check-in.component.html',
   styleUrls: ['./tab-check-in.component.scss'],
   standalone: true,
+  imports: [
+    PrimeNGModule
+  ]
 })
 export class TabCheckInComponent {
 
