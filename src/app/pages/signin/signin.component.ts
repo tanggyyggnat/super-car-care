c89bc67eb6218cb07c02d469a570c05e8a9992f2
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-10 23:54:06 +0700

    done signin service

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { AuthService } from 'src/app/service/auth/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent {
-  value: string | undefined;
+  accountName = '';
+  password = '';
+
+  constructor(
+    private router: Router,
+    private messageService: MessageService,
+    private authService: AuthService
+  ) { }
+
+  login() {
+    this.authService.
+      login(this.accountName, this.password)
+      .subscribe({
+        next: (res: any) => {
+          localStorage.setItem('user', JSON.stringify(res));
+          this.messageService.add({
+            severity: 'success',
+            summary: 'สำเร็จ',
+            detail: 'เข้าสู่ระบบสำเร็จ'
+          });
+          this.router.navigate(['/']);
+        },
+        error: (err: any) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'ล้มเหลว',
+            detail: err.error.error
+          });
+        }
+      });
+  }
 }
