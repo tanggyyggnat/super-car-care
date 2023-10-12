3ae63be8b0274fcc872886352cc774c41639c8d1
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-13 00:15:01 +0700

    update

--- src/app/pages/booking/booking.component.ts
+++ src/app/pages/booking/booking.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 //Service
 import { InventoryService } from 'src/app/service/inventory/inventory.service';
-import {MessageService} from "primeng/api";
+import { MessageService } from "primeng/api";
 
 @Component({
   selector: 'app-booking',
@@ -10,34 +10,23 @@ import {MessageService} from "primeng/api";
   styleUrls: ['./booking.component.scss']
 })
 export class BookingComponent {
-  typeService: any[] | undefined;
+  typeService: any[] = [];
 
-  products: any[]=[
-    {time:"09:00", status:"ว่าง"},
-    {time:"10:00", status:"ไม่ว่าง"},
-    {time:"11:00", status:"ว่าง"},
-    {time:"13:00", status:"ว่าง"},
-    {time:"14:00", status:"ไม่ว่าง"},
-    {time:"15:00", status:"ว่าง"},
+  products: any[] = [
+    { time: "09:00", status: "ว่าง" },
+    { time: "10:00", status: "ไม่ว่าง" },
+    { time: "11:00", status: "ว่าง" },
+    { time: "13:00", status: "ว่าง" },
+    { time: "14:00", status: "ไม่ว่าง" },
+    { time: "15:00", status: "ว่าง" },
   ];
-  selectedService: any | undefined;
-  date: Date | undefined;
-  listInventory: any[] = [];
-  dateService: any[] | undefined;
+
   constructor(
     private messageService: MessageService,
-    private inventoryService: InventoryService
-  )
-  {
-    this.getInventory();
+  ) {
   }
 
-  getInventory() {
-    this.inventoryService.getInventory().subscribe((res: any) => {
-      this.listInventory = res;
-    })
-  }
-  getSeverity(status: string):any {
+  getSeverity(status: string): any {
     switch (status) {
       case 'ว่าง':
         return 'success';
@@ -46,10 +35,5 @@ export class BookingComponent {
     }
   }
   ngOnInit() {
-    this.typeService = [
-        { name: 'Car Wash', code: 'CW' },
-        { name: 'Car Maintenance', code: 'CM' },
-        { name: 'Check Car Condition', code: 'CCC' }
-    ];
   }
 }
