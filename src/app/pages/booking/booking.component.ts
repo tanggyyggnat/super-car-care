0c4551ee71ba78c9e8c99a555d525742c902a321
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-14 03:44:31 +0700

    done booking

--- src/app/pages/booking/booking.component.ts
+++ src/app/pages/booking/booking.component.ts
@@ -3,6 +3,8 @@ import { MessageService } from "primeng/api";
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from 'src/app/service/account/account.service';
 import { ServiceTypeService } from 'src/app/service/serviceType/service-type.service';
+import { BookingService } from 'src/app/service/booking/booking.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-booking',
@@ -10,39 +12,61 @@ import { ServiceTypeService } from 'src/app/service/serviceType/service-type.ser
   styleUrls: ['./booking.component.scss']
 })
 export class BookingComponent {
+
+  showTimeTable: boolean = false;
+
   user: any = {};
   customer: any = {};
   typeService: any = [];
+  listBooking: any = [];
+  timeService: any = [];
   showValid: boolean = true;
 
   formData: FormGroup = new FormGroup({
-    custId: new FormControl('',[Validators.required]),
-    custName: new FormControl('',[Validators.required]),
-    custSurname: new FormControl('',[Validators.required]),
-    custEmail: new FormControl('',[Validators.required]),
-    custPhone: new FormControl('',[Validators.required]),
-    licensePlate: new FormControl('',[Validators.required]),
-    carBrand: new FormControl('',[Validators.required]),
-    serviceTypeId: new FormControl('',[Validators.required]),
-    appointmentDateTime: new FormControl(new Date(),[Validators.required]),
-    description: new FormControl('',[Validators.required]),
+    custId: new FormControl(null, [Validators.required]),
+    custName: new FormControl(null, [Validators.required]),
+    custSurname: new FormControl(null, [Validators.required]),
+    custEmail: new FormControl(null, [Validators.required]),
+    custPhone: new FormControl(null, [Validators.required]),
+    licensePlate: new FormControl(null, [Validators.required]),
+    carBrand: new FormControl(null, [Validators.required]),
+    serviceTypeId: new FormControl(null, [Validators.required]),
+    appointmentDateId: new FormControl(null, [Validators.required]),
+    appointmentDateTime: new FormControl(null, [Validators.required]),
+    description: new FormControl(null, [Validators.required]),
   });
 
-  products: any[] = [
-    { time: "09:00", status: "Not available" },
-    { time: "10:00", status: "Available" },
-    { time: "11:00", status: "Not available" },
-    { time: "13:00", status: "Available" },
-    { time: "14:00", status: "Available" },
-    { time: "15:00", status: "Not available" },
+  listTime: any[] = [
+    { id: 1, time: "09:00", status: "Available" },
+    { id: 2, time: "10:00", status: "Available" },
+    { id: 3, time: "11:00", status: "Available" },
+    { id: 4, time: "13:00", status: "Available" },
+    { id: 5, time: "14:00", status: "Available" },
+    { id: 6, time: "15:00", status: "Available" },
   ];
 
   constructor(
+    private router: Router,
     private messageService: MessageService,
     private accountService: AccountService,
     private serviceTypeService: ServiceTypeService,
+    private bookingService: BookingService
   ) {
     this.user = JSON.parse(localStorage.getItem('user')!);
+
+    this.formData.controls['appointmentDateTime'].valueChanges.subscribe((value: any) => {
+      let year = new Date(value).getFullYear();
+      let month = new Date(value).getMonth();
+      let day = new Date(value).getDate() + 1;
+      let date = new Date(year, month, day).toISOString();
+      this.bookingService
+        .searchScheduleDateTime(date)
+        .subscribe((res: any) => {
+          this.showTimeTable = true;
+          this.listBooking = res;
+          this.getTime();
+        });
+    });
   }
 
   ngOnInit() {
@@ -68,6 +92,37 @@ export class BookingComponent {
       });
   }
 
+  getTime() {
+    this.resetlistTimeTable();
+    this.resetTimeService();
+    this.listBooking.forEach((element: any) => {
+      let index = this.listTime.findIndex((time: any) => time.id === element.appointmentDateId);
+      this.listTime[index].status = element.stepStatus ? 'Not Available' : 'Available';
+
+      this.timeService = this.timeService.filter((time: any) => {
+        return time.value !== this.listTime[index].time;
+      });
+    });
+  }
+
+  resetlistTimeTable() {
+    this.listTime.forEach((element: any) => {
+      element.status = 'Available';
+    });
+  }
+
+  resetTimeService() {
+    this.timeService = [
+      { value: "-- Select Time --" },
+      { id: 1, value: "09:00" },
+      { id: 2, value: "10:00" },
+      { id: 3, value: "11:00" },
+      { id: 4, value: "13:00" },
+      { id: 5, value: "14:00" },
+      { id: 6, value: "15:00" }
+    ]
+  }
+
   patchData() {
     this.formData.patchValue({
       custId: this.customer.id,
@@ -76,15 +131,6 @@ export class BookingComponent {
       custEmail: this.customer.custEmail,
       custPhone: this.customer.custPhone,
     });
-  }
-
-  getSeverity(status: string): any {
-    switch (status) {
-      case 'Available':
-        return 'success';
-      case 'Not available':
-        return 'warning';
-    }
   }
 
   invalid(control: string): boolean {
@@ -96,13 +142,40 @@ export class BookingComponent {
     return false;
   }
 
-  booking() {
-    let values = this.formData.value;
-    if (this.formData.invalid) {
-      this.showValid = false;
-      return;
-    }
-    console.log(values);
+  onRowSelect(event: any) {
+    this.formData.controls['appointmentDateId'].setValue(event.data.id);
   }
 
+  booking() {
+    let values = this.formData;
+    let datForm = this.formData.get('appointmentDateTime')?.value;
+    let year = new Date(datForm).getFullYear();
+    let month = new Date(datForm).getMonth();
+    let day = new Date(datForm).getDate() + 1;
+    let date = new Date(year, month, day);
+    values.controls['appointmentDateTime'].setValue(date.toISOString());
+    this.bookingService
+      .createBooking(values.value)
+      .subscribe({
+        next: (res: any) => {
+          this.router.navigate(['/history/detail', res.id]);
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Booking successfully',
+          });
+        },
+        error: (err: any) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: err.error.error,
+          });
+          if (!values.invalid) {
+            this.showValid = false;
+            return;
+          }
+        },
+      });
+  }
 }
