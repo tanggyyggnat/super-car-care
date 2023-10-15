33e0ab6ea3fb78af8e86060c57cf33ae54b44368
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-15 12:15:31 +0700

    update history

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,29 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import data from 'src/app/mockup/customer.json';
+import { BookingService } from 'src/app/service/booking/booking.service';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
 
-  products: any[] = [];
+  booking: any[] = [];
 
   constructor(
     private router: Router,
+    private bookingService: BookingService
   ) {
-    this.products = data
+  }
+
+  ngOnInit(): void {
+    this.getBooking();
+
+  }
+
+  getBooking() {
+    this.bookingService
+      .getBooking()
+      .subscribe((res: any) => {
+        this.booking = res;
+      });
   }
 
 
   getSeverity(status: string): any {
     switch (status) {
-      case 'success':
-        return 'success';
-      case 'in process':
+      case 'IN_PROCESS':
+        return 'info';
+      case 'WAITING':
         return 'warning';
+      case 'CANCEL':
+        return 'danger';
+      case 'COMPLETE':
+        return 'success';
     }
   }
 
