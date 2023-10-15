62bd6bd83b771a535bcdfde5c74f273531a388bb
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-15 16:07:38 +0700

    done tab payment

--- src/app/pages/history/history-detail-tab/tab-payment/tab-payment.component.ts
+++ src/app/pages/history/history-detail-tab/tab-payment/tab-payment.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { UploadEvent } from 'primeng/fileupload';
 import { PrimeNGModule } from 'src/app/modules/primeng.module';
+import { PaymentService } from 'src/app/service/payment/payment.service';
 
 @Component({
   selector: 'tab-payment',
@@ -13,23 +16,72 @@ import { PrimeNGModule } from 'src/app/modules/primeng.module';
   ]
 })
 export class TabPaymentComponent {
-
+  @Input() booking: any;
   value: number | undefined;
   mobilebankingMethod = false;
   cashMethod = false;
 
-  constructor() {
-    console.log(this.cashMethod)
-    console.log(this.mobilebankingMethod)
+  fileUpload: any;
+  fileName: string = "";
+
+  // For mobile banking
+  bankAccount: string = "";
+  bankName: string = "";
+  paymentMethod: string = "";
+
+  constructor(
+    private messageService: MessageService,
+    private paymentService: PaymentService
+  ) {
   }
 
   selected(method: string) {
     if (method === 'CASH') {
       this.cashMethod = true;
       this.mobilebankingMethod = false;
+      this.paymentMethod = "CASH";
     } else if (method === 'BANKING') {
       this.cashMethod = false;
       this.mobilebankingMethod = true;
+      this.paymentMethod = "MOBILE_BANKING";
     }
+  }
+
+  onUpload(event: any) {
+    this.fileUpload = event.files[0];
+    this.fileName = this.fileUpload.objectURL.changingThisBreaksApplicationSecurity;
+  }
+
+  openFile() {
+    if (this.fileUpload) {
+      window.open(this.fileUpload.objectURL.changingThisBreaksApplicationSecurity, '_blank');
+    }
+  }
+
+  payment() {
+    let data = {
+      paymentMethod: this.paymentMethod,
+      bankingName: this.bankName,
+      accountNumber: this.bankAccount,
+      paymentSlip: this.fileUpload.objectURL.changingThisBreaksApplicationSecurity
+    };
+    this.paymentService
+      .updatePayment(this.booking.id, data)
+      .subscribe({
+        next: (res: any) => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Payment success'
+          });
+        },
+        error: (err: any) => { 
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Payment failed'
+          });
+        }
+      });
   }
 }
