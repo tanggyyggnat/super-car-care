3a10710087cdb3c3f416c395328f8c8fe308bbf5
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-10-15 16:42:32 +0700

    done tab check in

--- src/app/pages/history/history-detail-tab/tab-check-in/tab-check-in.component.ts
+++ src/app/pages/history/history-detail-tab/tab-check-in/tab-check-in.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { PrimeNGModule } from 'src/app/modules/primeng.module';
 
 @Component({
@@ -7,9 +8,16 @@ import { PrimeNGModule } from 'src/app/modules/primeng.module';
   styleUrls: ['./tab-check-in.component.scss'],
   standalone: true,
   imports: [
+    CommonModule,
     PrimeNGModule
   ]
 })
 export class TabCheckInComponent {
+  @Input() booking: any;
+  user = JSON.parse(localStorage.getItem('user')!);
+
+  getQueue(id: number) {
+    return String(id).padStart(6, '0');
+  }
 
 }
